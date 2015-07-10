f157c434a94802e1ebd15f72186de21eb5dee287
MKS-students <mks.students@hackreactor.com>
2015-07-09 21:04:11 -0700

    "Implements rooks solutions method"

--- src/solvers.js
+++ src/solvers.js
@@ -29,7 +29,40 @@
 // Termination case: when loop inside of recursive function reaches n iterations
 
 window.findNRooksSolution = function(n) {
-  // create solutions collection
+/*  // create solutions collection
+  var solutions = [];
+  // make new board
+  var board = new Board({n: n});
+  // count Rooks placed
+  var numRooks = 0;
+  // define recursive function
+  var recursiveRooks = function(rowNum) {
+    // initializa a count
+    var rowNum = rowNum || 0;
+
+    // loop over indeces
+    for (var colNum = 0; colNum < n; colNum++) {
+      // place piece at position x,y (i.e count, i)
+      board.setPiece(rowNum, colNum, 1);
+      // check for conflict
+      if (!board.hasAnyRooksConflicts()) {
+        // increment rowNum and recurse
+        // numRooks++;
+        rowNum++
+
+        // base case: has counter exceeded n
+        if (rowNum === n) {
+          solutions = board.allRows();
+          return;
+        }
+
+        // console.log(board.allRows());
+        recursiveRooks(rowNum+1);
+      } else {
+        board.setPiece(rowNum, colNum, 0);
+      }
+    }*/
+      // create solutions collection
   var solutions = [];
   // make new board
   var board = new Board({n: n});
@@ -54,7 +87,7 @@ window.findNRooksSolution = function(n) {
       if (!board.hasAnyRooksConflicts()) {
         // increment rowNum and recurse
         numRooks++;
-        console.log(board.allRows());
+        // console.log(board.allRows());
         recursiveRooks(rowNum+1);
       }
       else {
@@ -75,8 +108,46 @@ window.findNRooksSolution = function(n) {
 // Return the number of nxn chessboards that exist, with n rooks placed such that none
 // of them can attack each other.
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+    if (n == 2) {
+      // debugger;
+    }
+  // initialize a count
+  var solutionCount = 0;
 
+  // initialize a new board
+  var board = new Board({n:n});
+
+  // write a recursive function that accepts rowNum as an parameter
+  var recursiveSolutionCount = function(rowNum) {
+    // iterate over columns (i.e. indeces in rows, i.e. the y axis)
+    console.log('rowNum', rowNum);
+    for (var y = 0; y < n; y++) {
+      // set a piece
+      board.togglePiece(rowNum, y);
+      // check for conflicts
+
+      // if no conflict
+      if (!board.hasAnyRooksConflicts()) {
+        // increment rowNum
+        // rowNum++;
+        // if rowNum equals n
+        if (rowNum+1 === n) {
+          // increment solutions count
+          solutionCount++;
+          board.togglePiece(rowNum, y);
+          // return
+          return;
+        }
+
+        // otherwise recurse with new rowNum
+        recursiveSolutionCount(rowNum+1);
+      }
+      // if conflict, toggle piece and allow the loop to advance
+      board.togglePiece(rowNum, y);
+    }
+  };
+  // invoke recursive function
+  recursiveSolutionCount(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
